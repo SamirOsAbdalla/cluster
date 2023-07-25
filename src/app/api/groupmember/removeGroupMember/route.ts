051239bcf5b72594a9ad7e27d1601b9e780ef931
samir01 <samir.os.abdalla@gmail.com>
2023-07-24 18:08:11 -0700

    Deleting a group now deletes all tasks along with it

--- src/app/api/groupmember/removeGroupMember/route.ts
+++ src/app/api/groupmember/removeGroupMember/route.ts
@@ -9,7 +9,6 @@ export async function POST(request: Request) {
     const body: RequestBody = await request.json()
     const id = new mongoose.Types.ObjectId(body.groupId)
     const response = await GroupModel.findOneAndUpdate({ _id: id }, { $pull: { members: { memberEmail: body.userEmail } } })
-    console.log(response)
     if (response) {
         return new Response(JSON.stringify(response))
     }
