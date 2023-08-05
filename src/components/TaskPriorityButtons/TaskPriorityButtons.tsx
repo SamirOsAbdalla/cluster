5578cd474d4f01b1d3108b090b73ea14a5559116
samir01 <samir.os.abdalla@gmail.com>
2023-08-05 11:11:23 -0700

    Created seperate component for buttons to reduce code bloat in single file

---
+++ src/components/TaskPriorityButtons/TaskPriorityButtons.tsx
@@ -0,0 +1,25 @@
+import { TaskPriority } from "@/lib/mongo/models/TaskModel"
+import "./TaskPriorityButtons.css"
+import React, { Dispatch, SetStateAction } from 'react'
+
+interface Props {
+    setTaskPriority: Dispatch<SetStateAction<TaskPriority>>
+}
+export default function TaskPriorityButtons({ setTaskPriority }: Props) {
+    return (
+        <div className="tpbuttons__wrapper">
+            <button onClick={() => setTaskPriority("Low")} type="button" className="tpbuttons__button tpbuttons__low">
+                Low
+            </button>
+            <button onClick={() => setTaskPriority("Medium")} type="button" className="tpbuttons__button tpbuttons__medium">
+                Medium
+            </button>
+            <button onClick={() => setTaskPriority("High")} type="button" className="tpbuttons__button tpbuttons__high">
+                High
+            </button>
+            <button onClick={() => setTaskPriority("Urgent")} type="button" className="tpbuttons__button tpbuttons__urgent">
+                Urgent
+            </button>
+        </div>
+    )
+}
