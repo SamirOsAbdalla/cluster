31530bc3f7ac00581698948118718ddc7abcbfcb
samir01 <samir.os.abdalla@gmail.com>
2023-08-07 12:38:31 -0700

    Added comment section component to task display

--- src/components/DisplayedTask/DisplayedTask.tsx
+++ src/components/DisplayedTask/DisplayedTask.tsx
@@ -2,7 +2,7 @@ import React, { Dispatch, SetStateAction } from 'react'
 import "./DisplayedTask.css"
 import { TaskInterface } from '@/lib/mongo/models/TaskModel'
 import { AiFillCloseCircle, AiOutlineCloseCircle } from 'react-icons/ai'
-
+import CommentSection from '../CommentSection/CommentSection'
 interface Props {
     task: TaskInterface,
     setDisplayedTask: Dispatch<SetStateAction<TaskInterface>>
@@ -14,56 +14,60 @@ export default function DisplayedTask({ task, setDisplayedTask }: Props) {
                 Task Info
             </div>
             <AiFillCloseCircle onClick={() => setDisplayedTask({} as TaskInterface)} />
-            <div className='dt__information'>
-                <div className="dt__cell">
-                    <div>
-                        Name
-                    </div>
+            <div className="dt__main">
+                <div className='dt__information'>
+                    <div className="dt__cell">
+                        <div>
+                            Name
+                        </div>
 
-                    <div>
-                        {task.name}
+                        <div>
+                            {task.name}
+                        </div>
+                    </div>
+                    <div className="dt__cell">
+                        <div>
+                            Description
+                        </div>
+                        <div>
+                            {task.description}
+                        </div>
+                    </div>
+                    <div className="dt__cell">
+                        <div>
+                            Creator
+                        </div>
+                        <div>
+                            {task.creator.memberName}
+                        </div>
+                    </div>
+                    <div className="dt__cell">
+                        <div>
+                            Date Created
+                        </div>
+                        <div>
+                            {task.dateCreated.toString()}
+                        </div>
+                    </div>
+                    <div className="dt__cell">
+                        <div>
+                            Priority
+                        </div>
+                        <button>
+                            {task.priority}
+                        </button>
+                    </div>
+                    <div className="dt__cell">
+                        <div>
+                            Status
+                        </div>
+                        <div>
+                            {task.status}
+                        </div>
                     </div>
                 </div>
-                <div className="dt__cell">
-                    <div>
-                        Description
-                    </div>
-                    <div>
-                        {task.description}
-                    </div>
-                </div>
-                <div className="dt__cell">
-                    <div>
-                        Creator
-                    </div>
-                    <div>
-                        {task.creator.memberName}
-                    </div>
-                </div>
-                <div className="dt__cell">
-                    <div>
-                        Date Created
-                    </div>
-                    <div>
-                        {task.dateCreated.toString()}
-                    </div>
-                </div>
-                <div className="dt__cell">
-                    <div>
-                        Priority
-                    </div>
-                    <button>
-                        {task.priority}
-                    </button>
-                </div>
-                <div className="dt__cell">
-                    <div>
-                        Status
-                    </div>
-                    <div>
-                        {task.status}
-                    </div>
-                </div>
+                <CommentSection
+                    taskId={task._id!} />
             </div>
 
         </div>
