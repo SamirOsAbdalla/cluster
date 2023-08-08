3990c6f6ec5426d18da37d86210b86173ffe0fa9
samir01 <samir.os.abdalla@gmail.com>
2023-08-08 11:07:12 -0700

    Added css page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
+"use client"
 import GroupTable from "@/components/GroupTable/GroupTable"
 import Sidebar from "../components/Sidebar/Sidebar"
+import TaskTable from "@/components/TaskTable/TaskTable"
+import "./home.css"
+import { useState } from "react"
+import LoadingGroup from "@/components/LoadingGroup/LoadingGroup"
 
 export default function Home() {
+  const [loading, setLoading] = useState<boolean>(true)
+
   return (
-    <>
-      <GroupTable />
-    </>
+    <div className="home__wrapper">
+      {loading && <LoadingGroup />}
+      <GroupTable setLoading={setLoading} loading={loading} />
+      <TaskTable
+        loading={loading}
+        taskTableType="urgent"
+      />
+    </div>
   )
 }
