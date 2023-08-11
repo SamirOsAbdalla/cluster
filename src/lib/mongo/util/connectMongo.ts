1a68eb0e7484e20d14d125097e9dfd1636989fb4
samir01 <samir.os.abdalla@gmail.com>
2023-08-10 21:04:08 -0700

    Changed connect mongo to prevent mongo connection error

--- src/lib/mongo/util/connectMongo.ts
+++ src/lib/mongo/util/connectMongo.ts
@@ -3,29 +3,46 @@ import mongoose from "mongoose";
 const connection: any = {};
 
 
-async function connect() {
+// async function connect() {
+//     if (connection.isConnected) {
+//         return;
+//     }
+//     if (mongoose.connections.length > 0) {
+//         connection.isConnected = mongoose.connections[0].readyState;
+//         if (connection.isConnected === 1) {
+//             return;
+//         }
+//         await mongoose.disconnect();
+//     }
+
+//     let s: string = process.env.MONGODB_URI as string
+//     const db = await mongoose.connect(s);
+//     connection.isConnected = db.connections[0].readyState;
+// }
+
+// async function disconnect() {
+//     if (connection.isConnected) {
+//         if (process.env.NODE_ENV === 'production') {
+//             await mongoose.disconnect();
+//             connection.isConnected = false;
+//         }
+//     }
+// }
+
+
+const connect = async () => {
     if (connection.isConnected) {
+        // use cached connection when available
         return;
     }
-    if (mongoose.connections.length > 0) {
-        connection.isConnected = mongoose.connections[0].readyState;
-        if (connection.isConnected === 1) {
-            return;
-        }
-        await mongoose.disconnect();
-    }
+    try {
 
-    let s: string = process.env.MONGODB_URI as string
-    const db = await mongoose.connect(s);
-    connection.isConnected = db.connections[0].readyState;
-}
+        const DB_URI = process.env.MONGODB_URI as string
+        const dbConnection = await mongoose.connect(DB_URI);
+        connection.isConnected = dbConnection.connections[0].readyState;
+    } catch (err) {
 
-async function disconnect() {
-    if (connection.isConnected) {
-        if (process.env.NODE_ENV === 'production') {
-            await mongoose.disconnect();
-            connection.isConnected = false;
-        }
     }
-}
-export const db = { connect, disconnect };
+};
+
+export const db = { connect };
