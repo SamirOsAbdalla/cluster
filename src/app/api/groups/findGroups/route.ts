8aff3027a9998c6fa25bcc15e215cb92cef71e8b
samir01 <samir.os.abdalla@gmail.com>
2023-08-10 21:10:15 -0700

    Used next provided response

--- src/app/api/groups/findGroups/route.ts
+++ src/app/api/groups/findGroups/route.ts
@@ -9,11 +9,12 @@ export async function POST(request: Request) {
     await db.connect()
 
 
-    const body: MemberInterface = await request.json()
+    const body: Omit<MemberInterface, "profilePicture"> = await request.json()
     const allGroups = await GroupModel.find({ members: { $elemMatch: { memberEmail: `${body.memberEmail}` } } })
 
     if (allGroups) {
-        return new Response(JSON.stringify(allGroups))
+        // return new Response(JSON.stringify(allGroups))
+        return NextResponse.json(allGroups)
     }
 
     return new Response(JSON.stringify(null))
