a80d08e0cc483243bbe4d6073cea4c43af91049b
samir01 <samir.os.abdalla@gmail.com>
2023-08-14 07:51:52 -0700

    Login form is now stylized and loading animations have been added to the buttons

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -14,12 +14,16 @@ import { useRouter } from "next/navigation"
 import UserModel from "@/lib/mongo/models/UserModel"
 import ErrorMessage from "../ErrorMessage/ErrorMessage"
 import LoadingSpinner from "../LoadingSpinner/LoadingSpinner"
+import { FaUserAstronaut } from "react-icons/fa"
 interface LoginProps {
     type: string
 }
 export default function LoginForm({ type }: LoginProps) {
     const router = useRouter()
     const [loading, setLoading] = useState<boolean>(false)
+    const [demo1Loading, setDemo1Loading] = useState<boolean>(false)
+    const [demo2Loading, setDemo2Loading] = useState<boolean>(false)
+
     const [email, setEmail] = useState<string>("")
     const [firstName, setFirstName] = useState<string>("")
     const [lastName, setLastName] = useState<string>("")
@@ -29,8 +33,26 @@ export default function LoginForm({ type }: LoginProps) {
     useEffect(() => {
         setError("")
     }, [])
+
+    const setButtonsDisabled = () => {
+        const button1 = document.querySelector(".demouser__button1") as HTMLButtonElement
+        const button2 = document.querySelector(".demouser__button2") as HTMLButtonElement
+        const loginButton = document.querySelector(".login__button") as HTMLButtonElement
+
+        if (button1) {
+            button1.disabled = true;
+        }
+        if (button2) {
+            button2.disabled = true
+        }
+        loginButton.disabled = true;
+    }
+
+    const disableLoadings = () => {
+        setLoading(false);
+    }
+
     const handleLogin = async () => {
-        setLoading(true)
         const result = await signIn("credentials", {
             email: email,
             password: password,
@@ -44,17 +66,17 @@ export default function LoginForm({ type }: LoginProps) {
             setError("")
             router.push("/")
         }
-        setLoading(false)
+        disableLoadings()
     }
 
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
+        setButtonsDisabled()
         if (type == "login") {
             handleLogin()
         }
         else {
-            setLoading(true)
             const name = firstName + " " + lastName
             const newUser = {
                 name,
@@ -76,18 +98,15 @@ export default function LoginForm({ type }: LoginProps) {
                 let tempError = "userExists"
                 setError(tempError)
             }
-            setLoading(false)
         }
-
     }
     return (
         <div className="login__form__wrapper">
             <div className="login__form__container">
-                {loading && <LoadingSpinner type="login" />}
                 <div className="login__picture__container">
                     <div className="login__logo__container">
 
-                        <BsFillPersonFill className="login__logo" />
+                        <FaUserAstronaut className="login__logo" />
                     </div>
                 </div>
 
@@ -121,7 +140,8 @@ export default function LoginForm({ type }: LoginProps) {
                                     onChange={(e: React.ChangeEvent<HTMLInputElement>) => (setFirstName(e.target.value))}
                                     placeholder="Enter first name"
                                     type="text"
-                                    required />
+                                    required
+                                    className="login__input" />
                             </div>
                             <div className="form__item">
                                 <BsPerson className="form__icon" />
@@ -130,6 +150,7 @@ export default function LoginForm({ type }: LoginProps) {
                                     onChange={(e: React.ChangeEvent<HTMLInputElement>) => (setLastName(e.target.value))}
                                     placeholder="Enter last name"
                                     type="text"
+                                    className="login__input"
                                     required />
                             </div>
                         </div>
@@ -141,6 +162,7 @@ export default function LoginForm({ type }: LoginProps) {
                             onChange={(e: React.ChangeEvent<HTMLInputElement>) => (setEmail(e.target.value))}
                             placeholder="Enter email"
                             type="email"
+                            className="login__input"
                             required />
                     </div>
                     <div className="form__item">
@@ -150,24 +172,57 @@ export default function LoginForm({ type }: LoginProps) {
                             onChange={(e: React.ChangeEvent<HTMLInputElement>) => (setPassword(e.target.value))}
                             placeholder="Enter password"
                             type="password"
+                            className="login__input"
                             required />
                     </div>
                     {type == "login" ?
-                        <button type="submit" className="login__button">Login</button>
+                        <button onClick={() => {
+                            setLoading(true)
+                            setDemo1Loading(false)
+                            setDemo2Loading(false)
+                        }} type="submit" className="login__button">
+                            {loading ? <LoadingSpinner type="button" /> : "Login"}
+                        </button>
                         :
-                        <button type="submit" className="login__button">Sign up</button>
+                        <button onClick={() => { setLoading(true) }} type="submit" className="login__button">
+                            {loading ? <LoadingSpinner type="button" /> : "Sign up"}
+                        </button>
                     }
+                    {type == "login" ?
+                        <div className="demouser__buttons">
+                            <button onClick={
+                                () => {
+                                    setDemo1Loading(true)
+                                    setEmail("testuser1@gmail.com")
+                                    setPassword("test")
+                                }
+                            } className="demouser__button demouser__button1">
+                                {demo1Loading ? <LoadingSpinner type="button" /> : "Demo User 1"}
+                            </button>
+                            <button className="demouser__button demouser__button2"
+                                onClick={
+                                    () => {
+                                        setDemo2Loading(true)
+                                        setEmail("testuser2@gmail.com")
+                                        setPassword("test")
+                                    }
+                                }
+                            >
+                                {demo2Loading ? <LoadingSpinner type="button" /> : "Demo User 2"}
+                            </button>
+                        </div> :
+                        <></>}
                 </form>
                 {type == "login" ?
                     <div className="signup">
                         <span>Don't have an account?</span>
-                        <Link href="/signup"> Sign up</Link>
+                        <Link href="/signup" className="sign__link"> Sign up</Link>
                     </div>
                     :
                     <div>
                         <div className="signup">
                             <span>Already have an account?</span>
-                            <Link href="/signIn"> Sign in</Link>
+                            <Link href="/signIn" className="sign__link"> Sign in</Link>
                         </div>
                     </div>
                 }
