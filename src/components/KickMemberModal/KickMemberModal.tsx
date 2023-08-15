8f349763de91638be2e91ffe2d620856ce720cc0
samir01 <samir.os.abdalla@gmail.com>
2023-08-14 18:17:10 -0700

    General modal styling

--- src/components/KickMemberModal/KickMemberModal.tsx
+++ src/components/KickMemberModal/KickMemberModal.tsx
@@ -1,8 +1,10 @@
 import { MemberInterface } from "@/lib/mongo/models/GroupModel";
 import "./KickMemberModal.css"
 import React from 'react'
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import { TaskMemberType } from "@/lib/mongo/models/TaskModel";
+import { AiFillCloseCircle } from "react-icons/ai";
+import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
 
 interface Props {
     kickedMemberName: string;
@@ -14,8 +16,18 @@ interface Props {
 }
 export default function KickMemberModal({ groupId, kickedMemberName, kickedMemberEmail,
     setKickModalStatus, groupMembers, setGroupMembers }: Props) {
-    const kickMember = async () => {
 
+    const [loading, setLoading] = useState<boolean>(false)
+    const setButtonsDisabled = () => {
+        const confirmButton = document.querySelector(".kickmodal__kick") as HTMLButtonElement
+        const cancelButton = document.querySelector(".kickmodal__cancel") as HTMLButtonElement
+        confirmButton.disabled = true
+        cancelButton.disabled = true
+    }
+
+    const kickMember = async () => {
+        setButtonsDisabled()
+        setLoading(true)
         const kickMemberBody = {
             userEmail: kickedMemberEmail,
             groupId
@@ -35,18 +47,20 @@ export default function KickMemberModal({ groupId, kickedMemberName, kickedMembe
             const filteredGroupMembers = groupMembers.filter(groupMember => groupMember.memberEmail != kickedMemberEmail)
             setGroupMembers(filteredGroupMembers)
             setKickModalStatus("closed")
+            setLoading(false)
         } else {
             //throw error
         }
     }
     return (
         <div className="kickmodal__wrapper">
+            <AiFillCloseCircle onClick={() => setKickModalStatus("closed")} className="kickmodal__close" />
             <div className="kickmodal__heading">
                 <p>Are you sure you want to kick <span>{kickedMemberName}</span></p>
             </div>
             <div className="kickmodal__buttons">
                 <button onClick={kickMember} className="kickmodal__button kickmodal__kick">
-                    Kick
+                    {loading ? <LoadingSpinner type="button" /> : "Kick"}
                 </button>
                 <button onClick={() => setKickModalStatus("closed")} className="kickmodal__button kickmodal__cancel">
                     Cancel
