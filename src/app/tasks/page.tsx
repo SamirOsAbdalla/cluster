ddba3441b7a3f8a753e304be1cc88274497e0ac4
samir01 <samir.os.abdalla@gmail.com>
2023-10-14 17:06:39 -0700

    Altered task page styling to conform to new site theme

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -6,6 +6,8 @@ import { useEffect, useState } from 'react'
 import { useSession } from 'next-auth/react'
 import { TaskInterface } from '@/lib/mongo/models/TaskModel'
 import TaskTable from '@/components/TaskTable/TaskTable'
+import PageWrapper from '@/components/PageWrapper/PageWrapper'
+import Header from '@/components/Header/Header'
 export default function Tasks() {
 
     const session = useSession()
@@ -14,8 +16,14 @@ export default function Tasks() {
     const [tasks, setTasks] = useState<TaskInterface[]>([])
 
     return (
-        <div className="taskpage__wrapper">
-            <TaskTable taskTableType="user" />
-        </div>
+        <PageWrapper>
+            <div className="taskpage__wrapper">
+                <Header headerText='TASKS' />
+                <div className="taskpage__table">
+                    <TaskTable taskTableType="user" />
+                </div>
+            </div>
+        </PageWrapper>
+
     )
 }
