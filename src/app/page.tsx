999e85ee0ed2b0a5b464611b99eea59f0cfe71dd
samir01 <samir.os.abdalla@gmail.com>
2023-10-14 17:08:33 -0700

    Changed home page styling to match new light theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,20 +6,25 @@ import "./home.css"
 import { useState } from "react"
 import LoadingGroup from "@/components/LoadingGroup/LoadingGroup"
 import { GroupInterface } from "@/lib/mongo/models/GroupModel"
-
+import PageWrapper from "../components/PageWrapper/PageWrapper"
+import Header from "@/components/Header/Header"
 export default function Home() {
   const [loading, setLoading] = useState<boolean>(true)
   const [taskGroups, setTaskGroups] = useState<GroupInterface[]>([])
   const [fetchGroup, setFetchGroup] = useState<boolean>(false)
   return (
-    <div className="home__wrapper">
-      {loading && <LoadingGroup type="groups" />}
-      <GroupTable setLoading={setLoading} setFetchGroup={setFetchGroup} loading={loading} />
-      {fetchGroup == true &&
-        <TaskTable
-          loading={loading}
-          taskTableType="urgent"
-        />}
-    </div>
+    <PageWrapper>
+      <Header headerText={"DASHBOARD"} />
+      <div className="home__wrapper">
+        {loading && <LoadingGroup type="groups" />}
+        <GroupTable setLoading={setLoading} setFetchGroup={setFetchGroup} loading={loading} />
+        {fetchGroup == true &&
+          <TaskTable
+            loading={loading}
+            taskTableType="urgent"
+          />}
+      </div>
+    </PageWrapper>
+
   )
 }
